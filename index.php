4997d92a964314f79dd8a1ac6fcb1ed412907999
pasha <pavle.prezent@gmail.com>
2020-03-23 16:12:00 +0200

    hw 5.2

--- index.php
+++ index.php
@@ -3,10 +3,20 @@
 $PDO = new PDO('mysql:host=mysql;dbname=docker_test', 'root', '123');
 
 class Currency {
-	private $isoCode;
-}
+	
+	private $isoCode;                      //Приватное свойство
+	
+	public function __construct($a) {      //Конструктор класса
+		$a = numfmt_create( 'ru_RU', NumberFormatter::CURRENCY );
+		$this->setB($a);
+	}
+	
+	public function getB() {               //Гэттер
+		return $this->isoCode;
+	}
+	
+	public function setB() {              //Сеттер
+		$this->isoCode = $a;
+	}
 
-Class Money {
-	private $amount;
-	private $currency;
 }

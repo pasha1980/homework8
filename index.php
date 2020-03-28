0a3d08ef5b811e7f7e6d51242b7139f0cad9f3d9
pasha <pavle.prezent@gmail.com>
2020-03-28 15:12:45 +0200

    homework 7.5

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
 
 require_once 'my_autoloader/autoload.php';           // Собственный автозагрузчик (упрощённый)
 
-/* Выводит SELECT * FROM <table> WHERE id = :id
+/* Выводит SELECT * FROM <table> WHERE ...
  * 1 аргумент - *
  * 2 аргумент - <table>
- * 3 аргумент - id
+ * Остальные аргументы - в зависимости от метода, который вызывается
  * ----------------------------------------------------------------------
- * Работает только с названиями методов, приведённых в примере к домашке
+ * Например:
+ * Метод: findBetweenCreatedAt(*, <table>, $startDate, $endDate);
+ * Вернёт - SELECT * FROM <table> WHERE created_at BETWEEN $start_date AND $end_date
+ * ----------------------------------------------------------------------
+ * !!! Работает только с названиями методов, приведённых в примере к домашке
  */
 
 \App\SQL_select::findByEmailAndByStatus('Name' , "Students", '1' , '3');
